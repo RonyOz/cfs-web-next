ac68d0f5864e4e9b42078bd76a542553bfe8ab1d
ronyoz <ronyfarido@gmail.com>
2025-11-09 15:19:33 -0500

    feat: update next.config.ts with image domains and environment variables

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['cfs-api.onrender.com', 'localhost'],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
